484062a9e7617f604870c72019d9ad8c8754b279
高 俊峰 [WFE] <junfeng.gao@ingeek.com>
2021-01-12 10:45:28 +0800

    add log

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -8,7 +8,7 @@ const importLocal = require('import-local');
 
 // 使用本地文件
 if (importLocal(__filename)) {
-    require('import-local').info('cli', '正在使用 berners-cli 本地版本');
+    require('npmlog').info('cli', '正在使用 berners-cli 本地版本');
 } else {
 // 使用全局文件
     require('../lib')(process.argv.slice(2));// 传入命令行参数
